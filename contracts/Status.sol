39fa796b9de3bebb68f133b6f53c76e1c3e85a10
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-04-19 18:52:46 -0700

    Library & OSS files (#5)
    - Create Status.sol
    - Some very basic JS helpers

---
+++ contracts/Status.sol
@@ -0,0 +1,83 @@
+pragma solidity ^0.4.21;
+
+library Status {
+    enum Category {
+        Generic,
+        Permission,
+        Match,
+        Offer,
+        Availability,
+
+        x50,
+        x60,
+        x70,
+        x80,
+        x90,
+
+        AppCategory,
+
+        xB0,
+        xC0,
+        xD0,
+
+        Cryptography,
+        OffChain
+    }
+
+    enum Reason {
+        Failure,
+        Success,
+        Acceptance,
+        AwaitingOthers,
+        ActionRequired,
+
+        x05,
+        x06,
+        x07,
+        x08,
+        x09,
+
+        AppReason,
+
+        x0B,
+        x0C,
+        x0D,
+        x0E,
+
+        Info
+    }
+
+    function toByte(Category _category, Reason _reason) private pure returns (byte code) {
+        return (_category << 4) + _reason;
+    }
+
+    // Get nibbles
+
+    function categoryOf(byte _status) private pure returns (uint8 category) {
+        return _status >> 4;
+    }
+
+    function reasonOf(byte _status) private pure returns (uint8 reason) {
+        return _status & hex"0F";
+    }
+
+    // Check common statuses
+
+    function isFailure(byte _status) private pure returns (bool) {
+      return (reasonOf(_status) == 0);
+    }
+
+    function isOk(byte _status) private pure returns (bool) {
+        return (reasonOf(_status) == 1);
+    }
+
+    // `require`s
+
+    function requireOk(byte _status) private pure {
+        require(isOk(_status));
+    }
+
+    function requireOk(byte _status, string message) private pure {
+        require(isOk(_status), message);
+    }
+}
