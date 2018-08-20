83307a206344d68308ab3675c18db279dc74503a
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-08-19 23:46:57 -0700

    Add content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'ERC1066: Ethereum Status Codes',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify',
+  ],
 }
