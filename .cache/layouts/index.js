ebed9b80b1237b603b5a9188da4997fa7cfe0e89
Jenn Cooper <cooper.jennl@gmail.com>
2018-09-28 18:38:21 -0700

    Update .gitignore to include .cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/expede/Documents/Volunteer/ethereum-status-codes/src/layouts/index.js"
-  import data from "/Users/expede/Documents/Volunteer/ethereum-status-codes/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
