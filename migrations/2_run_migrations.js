ce00c107e18b23dbf583928f4a2f0bce9245c679
Jenn Cooper <cooper.jennl@gmail.com>
2018-09-29 15:05:52 -0700

    Update migrations

--- migrations/2_run_migrations.js
+++ migrations/2_run_migrations.js
@@ -1,9 +1,9 @@
 var Status = artifacts.require('Status');
-var LocalizationPrefs = artifacts.require('LocalizationPrefs');
-var StatusCodeLocalization = artifacts.require('StatusCodeLocalization');
+var LocalizationPreferences = artifacts.require('LocalizationPreferences');
+var FissionLocalization = artifacts.require('FissionLocalization');
 
 module.exports = async (deployer) => {
   await deployer.deploy(Status);
-  const msgr = await deployer.deploy(StatusCodeLocalization);
-    await deployer.deploy(LocalizationPrefs, StatusCodeLocalization.address);
+  const msgr = await deployer.deploy(FissionLocalization);
+  await deployer.deploy(LocalizationPreferences, FissionLocalization.address);
 };
