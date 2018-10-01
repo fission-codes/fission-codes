01302a0527f054c11d233d87782721509762ca98
Jenn Cooper <cooper.jennl@gmail.com>
2018-10-01 13:39:41 -0700

    Formatting

--- contracts/localization/FissionLocalization.sol
+++ contracts/localization/FissionLocalization.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.4.24;
 import "../../node_modules/ethereum-localized-messaging/contracts/Localization.sol";
 
 contract FissionLocalization is Localization {
-  event FissionCode(bytes32 indexed code, string message);
+    event FissionCode(bytes32 indexed code, string message);
 
-  function log(bytes32 _code) public {
-    emit FissionCode(_code, dictionary_[_code]);
-  }
+    function log(bytes32 _code) public {
+        emit FissionCode(_code, dictionary_[_code]);
+    }
 }
