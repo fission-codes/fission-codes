ff266f29fd1a32c3d6a0ac4af82dbf6bb68485e3
Jenn Cooper <cooper.jennl@gmail.com>
2018-10-03 13:34:25 -0700

    Use BasicEnglishLocalization as default

--- migrations/2_run_migrations.js
+++ migrations/2_run_migrations.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 const Status = artifacts.require('Status');
 const LocalizationPreferences = artifacts.require('LocalizationPreferences');
-const FissionLocalization = artifacts.require('FissionLocalization');
+const BasicEnglishLocalization = artifacts.require('BasicEnglishLocalization');
 /* eslint-enable no-undef */
 
 module.exports = async (deployer) => {
   await deployer.deploy(Status);
-  const msgr = await deployer.deploy(FissionLocalization);
-  await deployer.deploy(LocalizationPreferences, msgr.address);
+  const localization = await deployer.deploy(BasicEnglishLocalization);
+  await deployer.deploy(LocalizationPreferences, localization.address);
 };
