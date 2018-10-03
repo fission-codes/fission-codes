b6d8f84195053d5d7a5262e9231271919c7578e8
Jenn Cooper <cooper.jennl@gmail.com>
2018-10-03 13:34:44 -0700

    Better naming

--- test/localization/fissionLocalization.spec.js
+++ test/localization/fissionLocalization.spec.js
@@ -5,22 +5,22 @@ const FissionLocalization = artifacts.require('FissionLocalization');
 /* eslint-enable no-undef */
 
 contract('FissionLocalization', async () => { // eslint-disable-line no-undef
-  let fissionLocalizationInstance;
+  let fissionLocalization;
 
   before(async () => {
-    fissionLocalizationInstance = await FissionLocalization.new();
-    await fissionLocalizationInstance.set('0x01', 'message');
+    fissionLocalization = await FissionLocalization.new();
+    await fissionLocalization.set('0x01', 'message');
   });
 
   describe('#log', () => {
     it('emits a FissionCode event', async () => {
-      const tx = await fissionLocalizationInstance.log('0x01');
+      const tx = await fissionLocalization.log('0x01');
 
       expect(tx.logs[0].event).to.equal('FissionCode');
     });
 
     it('emits the correct message', async () => {
-      const tx = await fissionLocalizationInstance.log('0x01');
+      const tx = await fissionLocalization.log('0x01');
 
       expect(tx.logs[0].args[1]).to.equal('message');
     });
