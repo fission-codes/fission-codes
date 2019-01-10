9d5d692aa80843c4bf8f277ed08306d7d3686b60
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-01-10 11:52:33 -0800

    Update with localization changes

--- contracts/localization/FissionLocalization.sol
+++ contracts/localization/FissionLocalization.sol
@@ -5,7 +5,7 @@ import "/ethereum-localized-messaging/contracts/Localization.sol";
 contract FissionLocalization is Localization {
     event FissionCode(bytes32 indexed code, string message);
 
-    function log(bytes32 _code) public {
-        emit FissionCode(_code, dictionary_[_code]);
+    function log(bytes32 code) public {
+        emit FissionCode(code, dictionary[code]);
     }
 }
