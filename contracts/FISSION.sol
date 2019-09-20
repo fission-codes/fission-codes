556429e62a858cbd61b1a1567ee91bb9a1cebebe
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-09-19 17:29:41 -0700

    bump solidity versin

--- contracts/FISSION.sol
+++ contracts/FISSION.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.8;
 
 import { LocalizationPreferences} from "/ethereum-localized-messaging/contracts/LocalizationPreferences.sol";
 import { FissionLocalization } from "./localization/FissionLocalization.sol";
--- contracts/examples/InsuranceValidator.sol
+++ contracts/examples/InsuranceValidator.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.8;
 
 import { AgeValidator } from "./AgeValidator.sol";
 import { FinancialValidator } from "./FinancialValidator.sol";
